198423ac4ed6abded35d36bd322fe2f2b47c0507
lucas werneck <>
2026-06-19 15:48:48 -0300

    Primeiro commit: Estrutura inicial do TManagement e README

--- database.py
+++ database.py
@@ -1,24 +1,17 @@
-from sqlalchemy import create_engine # Cria a conexão com o banco em nuvem
+from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
-#base para criar os modelos
-from sqlalchemy.orm import sessionmaker 
-#usada pra executar as linhas
+from sqlalchemy.orm import sessionmaker
 
-# URL de conexão oficial do seu banco Supabase
-# Nota: O '@' da senha foi trocado por '%40' para evitar erros de leitura na URL
-DATABASE_URL = "postgresql://postgres:WERNECK123%40@db.zifezehamdnhwohnvtbq.supabase.co:5432/postgres"
+# URL apontando para o Docker local
+DATABASE_URL = "postgresql://postgres:admin@localhost:5432/banco_projeto"
 
 engine = create_engine(DATABASE_URL)
- #a conexão em si com o PostgreSQL
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-Base = declarative_base() 
+Base = declarative_base()
 
 def get_db():
     db = SessionLocal()
     try:
         yield db
-        # entregar um valor, pausar e depois continuar de onde ela parou
     finally:
-        #garante que a sessão sempre feche
         db.close()
-    #Abre uma sessão
